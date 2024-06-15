38a6d92d8214b00d9ff8fd6b32f194d1d2f9f161
Lenovo 15 <information@com1software.com>
2024-06-15 16:49:48 -0400

    update with copilot changes

--- Serial/Go/main.go
+++ Serial/Go/main.go
@@ -1,16 +1,17 @@
-
 package main
 
 import (
 	"encoding/hex"
 	"fmt"
 	"log"
+	"math"
 	"strconv"
 
 	"go.bug.st/serial"
 )
 
 var xpos float64
+var ypos float64
 
 func main() {
 	fmt.Println("Test Multiport Serial Controller")
@@ -75,91 +76,41 @@ func main() {
 					// fmt.Printf(" %s %d %d", encodedStr, decimal, pos)
 
 					switch {
-							case pos == 2:
-			xpos = float64(decimal) / 32768.0 * 180.0
-			fmt.Printf(" x= %f ", xpos)
-			fmt.Printf(" xH= %d", decimal)
-		case pos == 3:
-			ypos = float64(decimal) / 32768.0 * 180.0
-			fmt.Printf(" y= %f ", ypos)
-			fmt.Printf(" yH= %d", decimal)
-		//			case pos == 2:
-		//				xpos = float64(decimal) / 32768.0 * 180.0
-		//				fmt.Printf(" x= %f ", xpos)
-		//				fmt.Printf(" xH= %d", decimal)
-		//			case pos == 3:
-		//				fmt.Printf(" xL= %d", decimal)
+					case pos == 2:
+						xpos = float64(decimal) / 32768.0 * 180.0
+						//fmt.Printf(" x= %f ", xpos)
+						//fmt.Printf(" xH= %d", decimal)
+					case pos == 3:
+						ypos = float64(decimal) / 32768.0 * 180.0
+						//fmt.Printf(" y= %f ", ypos)
+						//fmt.Printf(" yH= %d", decimal)
+						//			case pos == 2:
+						//				xpos = float64(decimal) / 32768.0 * 180.0
+						//				fmt.Printf(" x= %f ", xpos)
+						//				fmt.Printf(" xH= %d", decimal)
+						//			case pos == 3:
+						//				fmt.Printf(" xL= %d", decimal)
 					case pos == 4:
-						fmt.Printf(" yH= %d", decimal)
+					//	fmt.Printf(" yH= %d", decimal)
 					case pos == 5:
-						fmt.Printf(" yL= %d", decimal)
+						//		fmt.Printf(" yL= %d", decimal)
 					case pos == 6:
-						fmt.Printf(" zH= %d", decimal)
+						//		fmt.Printf(" zH= %d", decimal)
 					case pos == 7:
-						fmt.Printf(" zL= %d", decimal)
+						//		fmt.Printf(" zL= %d", decimal)
 
 					}
+
+					heading := math.Atan2(ypos, xpos) * 180 / math.Pi
+					if heading < 0 {
+						heading += 360
+					}
+					fmt.Printf("Heading: %f\n", heading)
 				}
-		
+
 			}
 		}
 
-			heading := math.Atan2(ypos, xpos) * 180 / math.Pi
-	if heading < 0 {
-		heading += 360
-	}
-	fmt.Printf("Heading: %f\n", heading)
-
-		
 	}
 
-}
-
-package main
-
-import (
-	"encoding/hex"
-	"fmt"
-	"log"
-	"math"
-	"strconv"
-
-	"go.bug.st/serial"
-)
-
-var xpos float64
-var ypos float64
-
-func main() {
-	// ... (rest of your code)
-
-	if mctl == 2 {
-		pos++
-
-		decimal, err := strconv.ParseInt(encodedStr, 16, 32)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		switch {
-		case pos == 2:
-			xpos = float64(decimal) / 32768.0 * 180.0
-			fmt.Printf(" x= %f ", xpos)
-			fmt.Printf(" xH= %d", decimal)
-		case pos == 3:
-			ypos = float64(decimal) / 32768.0 * 180.0
-			fmt.Printf(" y= %f ", ypos)
-			fmt.Printf(" yH= %d", decimal)
-		// ... (rest of your switch cases)
-		}
-	}
-
-	// Calculate the heading
-	heading := math.Atan2(ypos, xpos) * 180 / math.Pi
-	if heading < 0 {
-		heading += 360
-	}
-	fmt.Printf("Heading: %f\n", heading)
-
-	// ... (rest of your code)
 }
